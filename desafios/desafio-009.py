d10e0f10f5a464a506818685e8f806198f330fa8
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-03-26 14:34:14 -0300

    Desafio 009

---
+++ desafios/desafio-009.py
@@ -0,0 +1,17 @@
+#Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+x = int (input('Digite um número para ver sua tabuada: '))
+
+print('-' * 12)
+print('{} X {:2} = {:2}'. format(x, 1, x*1))
+print('{} X {:2} = {:2}'. format(x, 2, x*2))
+print('{} X {:2} = {:2}'. format(x, 3, x*3))
+print('{} X {:2} = {:2}'. format(x, 4, x*4))
+print('{} X {:2} = {:2}'. format(x, 5, x*5))
+print('{} X {:2} = {:2}'. format(x, 6, x*6))
+print('{} X {:2} = {:2}'. format(x, 7, x*7))
+print('{} X {:2} = {:2}'. format(x, 8, x*8))
+print('{} X {:2} = {:2}'. format(x, 9, x*9))
+print('{} X {:2} = {:2}'. format(x, 10, x*10))
+print('-' * 12)
+
