b1f59790ca3031c4c40102add061bcbade6bae26
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-03-27 09:03:17 -0300

    Accentuation correction

--- desafios/desafio-014.py
+++ desafios/desafio-014.py
@@ -4,4 +4,4 @@ celsius = float (input('Informe a temperatura em C°: '))
 fahrenheit = (celsius * 1.8) + 32
 
 
-print('A temperatura de {:.1f}°C corresponde a {:.1f}°F!.'. format(celsius, fahrenheit))
+print('A temperatura de {:.1f}°C corresponde a {:.1f}°F!'. format(celsius, fahrenheit))
