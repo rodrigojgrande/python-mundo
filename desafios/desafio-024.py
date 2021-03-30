ed084fa9be4b69c7323172d4c81f8a198031764f
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-03-30 17:19:05 -0300

    Refactor: Capitalize to Upper

--- desafios/desafio-024.py
+++ desafios/desafio-024.py
@@ -1,7 +1,7 @@
 #Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
 
 cidade = (input('Em que cidade você nasceu? ')).strip()
-cidade = cidade.capitalize()
+cidade = cidade.upper()
 primeiro = cidade.split()
 
-print('Santo' in primeiro[0])
+print('SANTO' in primeiro[0])
