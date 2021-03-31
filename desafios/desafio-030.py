1a5ce5520d6a001845e9f28348cb6ebb1899f19e
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-03-31 15:12:29 -0300

    Desafio 030

---
+++ desafios/desafio-030.py
@@ -0,0 +1,8 @@
+#Exercício Python 30: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+numero = int(input('Me diga um número qualquer: '))
+
+if ((numero % 2) == 0):
+    print('O número {} é PAR!'.format(numero))
+else: 
+    print('O número {} é ÍMPAR!'.format(numero))
