1393993f2e1b39a86ece00b487025b0af49b8d57
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-04-05 10:37:21 -0300

    Desafio 034

---
+++ desafios/desafio-034.py
@@ -0,0 +1,14 @@
+#Exercício Python 34: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+salario = float(input('Qual é o salário do funcionário? R$'))
+
+if ((salario > 1250)):
+    reajuste = salario * 1.10
+else: 
+    reajuste = salario * 1.15
+
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora'.format(salario, reajuste))
+
+#Outro modo de cálculo de porcentagem 
+#novo = salario + (salario * 15 /100)
+#novo = salario - (salario * 15 /100)
