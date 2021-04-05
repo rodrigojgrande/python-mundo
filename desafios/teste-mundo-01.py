c897bda4ffd14e124cb9917ef15027d4cf78f386
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-04-05 15:12:27 -0300

    Teste Mundo 01

---
+++ desafios/teste-mundo-01.py
@@ -0,0 +1,79 @@
+#01. Qual é o resultado calculado pelo Python para as expressões simples 19 // 2 e 19%2, respectivamente?
+print('01. Qual é o resultado calculado pelo Python para as expressões simples 19 // 2 e 19%2, respectivamente?')
+a = 19 // 2
+b = 19%2
+print('\033[1;32mR:', a,'e', b, '\033[m')
+
+print('\n')
+
+#02. Das opções abaixo, apenas uma NÃO É uma característica da linguagem Python. Marque a opção inválida da lista a seguir:
+print('02. Das opções abaixo, apenas uma NÃO É uma característica da linguagem Python. Marque a opção inválida da lista a seguir:')
+print('\033[1;32mR: É nativamente compilada\033[m')
+
+print('\n')
+
+#03. Considerando as variáveis n='José' e i=25, qual das opções abaixo é a única válida como uma string formatada que funcionaria em Python?
+print('03. Considerando as variáveis n=José e i=25, qual das opções abaixo é a única válida como uma string formatada que funcionaria em Python?')
+n = 'José'
+i = 25
+print('\033[1;32mR: Você se chama {} e tem {} anos de idade\033[m'.format(n, i))
+
+print('\n')
+
+#04. Qual das opções a seguir completa as lacunas da afirmação abaixo?
+print('04. Qual das opções a seguir completa as lacunas da afirmação abaixo?')
+print('A Linguagem Python foi criada no ano de ___ , pelo programador ______________. Das opções abaixo, a única que preenche corretamente as lacunas é:')
+print('\033[1;32mR: 1982 / Guido Van Hossum\033[m')
+
+print('\n')
+
+
+#05. Qual das opções a seguir é a única que NÃO É um tipo primitivo em Python?
+print('05. Qual das opções a seguir é a única que NÃO É um tipo primitivo em Python?')
+print('\033[1;32mR: double\033[m')
+
+print('\n')
+
+#06. Se o nosso programa Python precisar calcular a raiz quadrada de um número, seria interessante incluir qual linha como primeiro comando desse programa?
+print('06. Se o nosso programa Python precisar calcular a raiz quadrada de um número, seria interessante incluir qual linha como primeiro comando desse programa?')
+print('\033[1;32mR: from math import sqrt\033[m')
+
+print('\n')
+
+#07. Para o Python, os valores verdadeiro e falso são do tipo ________ e são representados pelos valores ________ e ________. Qual das opções a seguir é a única que contém as palavras que completam as lacunas da frase anterior, na ordem?
+print('07. Para o Python, os valores verdadeiro e falso são do tipo ________ e são representados pelos valores ________ e ________. Qual das opções a seguir é a única que contém as palavras que completam as lacunas da frase anterior, na ordem?')
+print('\033[1;32mR: bool / True / False\033[m')
+
+print('\n')
+
+#08. Considere a string x = 'curso de python no cursoemvideo'. Qual dos comandos abaixo retornaria a palavra 'curso'?
+print('08. Considere a string x = "curso de python no cursoemvideo". Qual dos comandos abaixo retornaria a palavra "curso"?')
+
+x = 'curso de python no cursoemvideo'
+print(x[:5])
+
+print('\033[1;32mR: x[:5]\033[m')
+
+print('\n')
+
+#09. Qual das opções a seguir permite ler a idade de uma pessoa em Python, já considerando o valor digitado como um número?
+print('09. Qual das opções a seguir permite ler a idade de uma pessoa em Python, já considerando o valor digitado como um número?')
+
+idade = int(input('Quantos anos você tem?'))
+
+print('\033[1;32mR:', idade, '\033[m')
+
+print('\n')
+
+#10. Considere a string s = 'prova de python'. A instrução len(s) retornaria qual valor?
+print('10. Considere a string s = "prova de python". A instrução len(s) retornaria qual valor?')
+
+s = 'prova de python'
+comprimento = len(s)
+
+print(comprimento)
+print('\033[1;32mR: 15\033[m')
+
+print('\n')
+
+
