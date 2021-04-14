2547ea9174f3796cceab8086f3afb563b229286a
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-04-14 17:03:49 -0300

    Teste Mundo 02

---
+++ desafios/teste-mundo-02.py
@@ -0,0 +1,59 @@
+#01. Qual é o jeito certo de verificar se a primeira letra de uma string n é uma vogal?
+print('01. Qual é o jeito certo de verificar se a primeira letra de uma string n é uma vogal?')
+print('\033[1;32mR: n[0] in aeiou\033[m')
+
+print('\n')
+
+#02. Se usarmos a estrutura for c in range(0, 5): no Python e colocarmos dentro do bloco um comando print(c), quais serão os valores impressos na tela do terminal?
+print('02. Se usarmos a estrutura for c in range(0, 5): no Python e colocarmos dentro do bloco um comando print(c), quais serão os valores impressos na tela do terminal?')
+print('\033[1;32mR: 0, 1, 2, 3, 4\033[m')
+
+print('\n')
+
+#03. Que comando podemos usar para gerar um número inteiro aleatório, entre 10 e 100?
+print('03. Que comando podemos usar para gerar um número inteiro aleatório, entre 10 e 100?')
+print('\033[1;32mR: n = randint(10, 100)\033[m')
+
+print('\n')
+
+#04. Qual é o comando em Python responsável por interromper um laço imediatamente?
+print('04. Qual é o comando em Python responsável por interromper um laço imediatamente?')
+print('\033[1;32mR: break\033[m')
+
+print('\n')
+
+#05. Para podermos gerar números aleatórios em nossos programas em Python, temos que importar a biblioteca correta, usando o comando:
+print('05. Para podermos gerar números aleatórios em nossos programas em Python, temos que importar a biblioteca correta, usando o comando:')
+print('\033[1;32mR: from random import randint\033[m')
+
+print('\n')
+
+#06. Qual das opções a seguir contém apenas estruturas de repetição (laços) em Python?:
+print('06. Qual das opções a seguir contém apenas estruturas de repetição (laços) em Python?:')
+print('\033[1;32mR: for / while\033[m')
+
+print('\n')
+
+#07. Qual dos comandos abaixo seria o mais adequado para ler uma única letra pelo teclado, transformando-a sempre em maiúsculas? Se por acaso o usuário digitar mais de uma letra, apenas a primeira será considerada.:
+print('07. Qual dos comandos abaixo seria o mais adequado para ler uma única letra pelo teclado, transformando-a sempre em maiúsculas? Se por acaso o usuário digitar mais de uma letra, apenas a primeira será considerada.')
+print('\033[1;32mR: resp = str(input(Digite uma letra: )).strip().upper()[0]\033[m')
+
+print('\n')
+
+#08. As condições aninhadas em Python são representadas pela estrutura:
+print('08. As condições aninhadas em Python são representadas pela estrutura:')
+print('\033[1;32mR: if: elif: else:\033[m')
+
+print('\n')
+
+#09. Em uma condição composta, qual das cláusulas de estrutura pode se repetir dentro de uma mesma condição?
+print('09. Em uma condição composta, qual das cláusulas de estrutura pode se repetir dentro de uma mesma condição?')
+print('\033[1;32mR: podemos ter vários elif: em um mesmo if composto\033[m')
+
+print('\n')
+
+#10. Ao usar uma estrutura condicional, o que determina se um comando faz parte de um determinado bloco?
+print('10. Ao usar uma estrutura condicional, o que determina se um comando faz parte de um determinado bloco?')
+print('\033[1;32mR: o bloco deve estar deslocado para a esquerda no código\033[m')
+
+print('\n')
