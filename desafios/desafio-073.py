1d1923ca3ca67569e0ecf9f3cacc2e33660878ba
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-04-15 16:30:52 -0300

    Add comentary

--- desafios/desafio-073.py
+++ desafios/desafio-073.py
@@ -1,4 +1,8 @@
 #Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+#a) Os 5 primeiros times.
+#b) Os últimos 4 colocados.
+#c) Times em ordem alfabética.
+#d) Em que posição está o time da Chapecoense.
 
 tupla = ('Astralis', 'Natus', 'Vincere', 'Team Vitality', 'BIG', 'G2 Esports', 'Heroic', 'Team Liquid', 'FURIA Esports', 'Fnatic', 'FaZe Clan', 'FURIA Esports', 'MIBR', 'GODSENT', 'O PLANO')
 
@@ -12,4 +16,3 @@ print(f'Os times em ordem alfabética: {sorted(tupla)}')
 print('-='*60)
 print(f'O MIBR está na {tupla.index("MIBR")+1}° posição')
 print('-='*60)
-print(f'{len(tupla)}')
